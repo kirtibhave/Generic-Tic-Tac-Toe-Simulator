4da1bac0d4a34bda09af87f4805359e8c55d1af6
kirtibhave <kirtibhave1000@gmail.com>
2020-03-14 12:32:25 +0530

    ADDED:Function to start fresh by resetting the board

--- genericTicTacToeSimulation.sh
+++ genericTicTacToeSimulation.sh
@@ -1,3 +1,22 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "********************Welcome to TicTacToe Simulation Problem********************"
+
+#CONSTANTS
+readonly size=$1
+ROW=$size
+COLUMN=$ROW
+
+#function is used to Start fresh by resetting the board
+function resetBoard(){
+	for ((row=1;row<=$ROW;row++))
+	do
+		for ((column=1;column<=$COLUMN;column++))
+		do
+			board[$row,$column]="-"
+		done
+			echo ${board[@]}
+	done
+}
+
+resetBoard
 
